95d408e9665bf8026f0eb82f327fb67b2c37cb97
Felipe Carrasco <felipe@bakers.dev>
2021-06-18 23:23:36 -0400

    <000> first project

---
+++ Store-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Store-app
+//
+//  Created by Pipe Carrasco on 18-06-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
